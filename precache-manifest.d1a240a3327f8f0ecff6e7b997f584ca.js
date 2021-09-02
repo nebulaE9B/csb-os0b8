9c7e02dbc8bcfeca739b4323fc324d3d826a8116
Code Sandbox <hello@codesandbox.io>
2021-09-02 07:55:38 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.d1a240a3327f8f0ecff6e7b997f584ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9560a7211e2ec4f14252419ec1f644cd",
+    "url": "https://nebulae9b.github.io/csb-os0b8/index.html"
+  },
+  {
+    "revision": "811fbef5bd3385391c87",
+    "url": "https://nebulae9b.github.io/csb-os0b8/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "ca031c3cc276d81bcadb",
+    "url": "https://nebulae9b.github.io/csb-os0b8/static/js/2.0c582926.chunk.js"
+  },
+  {
+    "revision": "811fbef5bd3385391c87",
+    "url": "https://nebulae9b.github.io/csb-os0b8/static/js/main.148157c0.chunk.js"
+  },
+  {
+    "revision": "b33ca1aac942e8f56a89",
+    "url": "https://nebulae9b.github.io/csb-os0b8/static/js/runtime~main.4604e10e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://nebulae9b.github.io/csb-os0b8/precache-manifest.d1a240a3327f8f0ecff6e7b997f584ca.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://nebulae9b.github.io/csb-os0b8/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
